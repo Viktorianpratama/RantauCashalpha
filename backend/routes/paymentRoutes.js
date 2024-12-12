c2c2e05d1f218856af4ace1fa251ec624b0fc455
Viktor Pratama <147905833+Viktorianpratama@users.noreply.github.com>
2024-12-13 01:53:13 +0900

    Update paymentRoutes.js

--- backend/routes/paymentRoutes.js
+++ backend/routes/paymentRoutes.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());  // Untuk memparse request body (termasuk senderEmail)
 
-const upload = multer({ dest: 'uploads/' });
+// const upload = multer({ dest: 'uploads/' });
 
 const router = express.Router();
 
