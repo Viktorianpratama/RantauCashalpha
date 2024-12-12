0c767fc8c4228507900f276787cc352b2d1ebd5c
Viktor Pratama <147905833+Viktorianpratama@users.noreply.github.com>
2024-12-13 02:04:48 +0900

    Update Signup.jsx

--- rantaucash/src/pages/Signup.jsx
+++ rantaucash/src/pages/Signup.jsx
@@ -19,7 +19,7 @@ const Signup = () => {
     }
   
     try {
-      await axios.post('http://localhost:5000/api/users/register', {
+      await axios.post('https://rantau-cashalpha-lemon.vercel.app/api/users/register', {
         name,
         email,
         password,
